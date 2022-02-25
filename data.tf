3ce45bff7fb5214e29f5d64f59457b359acca270
Ronald Schouw <mail@ronalt.nl>
2022-02-25 09:00:15 +0100

    filename changed

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 data "template_file" "cloudinit" {
-  template =  "${file("${path.module}/cloud-init-linux-cmc.tpl")}"
+  template =  "${file("${path.module}/cloud-init-linux.tpl")}"
   vars = {
     host_name = var.host_name
   }
@@ -8,7 +8,7 @@ data "template_file" "cloudinit" {
 
 resource "local_file" "cloudinit" {
   content  = data.template_file.cloudinit.rendered
-  filename = "cloud-init-linux-cmc.config"
+  filename = "cloud-init-linux.config"
  }
 
 data "template_cloudinit_config" "config" {
